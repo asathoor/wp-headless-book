d5155a84dbe6bce5e26f4c97e13063db161e8b4a
Per Thykjær Jensen <petj@eaaa.dk>
2021-07-24 20:05:08 +0200

    nav repair

--- js/js.js
+++ js/js.js
@@ -1,28 +1,27 @@
-(function() { // use strict function form
-'use strict'
-/**
- * file: js/js.js
- **/
+(function () { // use strict function form
+    'use strict'
+    
+  /**
+   * file: js/js.js
+   **/
 
- // dummy image
- const TestImage = function(message, w, h) {
-   return `
-   <figure class="figure">
+  // dummy image
+    const TestImage = function (message, w, h) {
+    return `<figure class="figure">
        <img src="https://via.placeholder.com/${w}x${h}?text=${message}" alt="dummy image" class="img-fluid">
        <figcaption class="figure-caption">Splendor Solis.</figcaption>
-   </figure>
-    `
- }
+   </figure>`
+  }
 
-// lorem ipsum generator
-function Lorem() {
-  return TestImage('Image', 500, 500) +
-    `
+  // lorem ipsum generator
+  function Lorem() {
+    return TestImage('Image', 500, 500) +
+      `
     <h2> Nicolas et Perenelle Flamel </h2>
     <p>
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione praesentium repellat cupiditate sunt,
-      libero esse nostrum saepe, iure molestias. Nam ut consectetur hic deleniti
-      adipisci in unde, molestias totam quasi.
+      libero esse nostrum saepe, iure molestias. Nam ut consectetur hic
+      deleniti adipisci in unde, molestias totam quasi.
     </p>
     <ul>
       <li> <a href="#"> Hyperlink </a>
@@ -37,13 +36,13 @@ function Lorem() {
       deleniti adipisci in unde, molestias totam quasi.
     </p>
    `
-}
+  }
 
-// add dummy content to the layout
-/*
-leftText.innerHTML = Lorem()
-for (let i=0;i<7;i++){ centerText.innerHTML += Lorem() }
-for (let i=0;i<3;i++){ rightText.innerHTML += Lorem() }
-hero.innerHTML = TestImage('Carousel', 2000, 400)
-*/
+  // add dummy content to the layout
+  /*
+  leftText.innerHTML = Lorem()
+  for (let i=0;i<7;i++){ centerText.innerHTML += Lorem() }
+  for (let i=0;i<3;i++){ rightText.innerHTML += Lorem() }
+  hero.innerHTML = TestImage('Carousel', 2000, 400)
+  */
 })(); // use strict function form
